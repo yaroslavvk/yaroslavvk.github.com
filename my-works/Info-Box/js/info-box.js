9ce8a5a74503be63fdf555846df28465bcd4f04c
yvk <yaroslavvk@outlook.com>
2015-05-26 21:02:00 +0300

    add info-box

---
+++ my-works/Info-Box/js/info-box.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+// (function() {
+// 	// function infoBox(targetNode, JSON) {
+// 	// 	// console.log(JSON);
+// 	// 	this.targetNode = targetNode;
+// 	// }
+// 	function infoBox(targetNode) {
+// 		// console.log(JSON);
+// 		this.targetNode = targetNode;
+// 	}
+
+// 	window.infoBox = infoBox;
+// })();
