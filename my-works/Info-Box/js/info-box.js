9e293cf383b47d250f20327fc734c03c1b34492e
yvk <yaroslavvk@outlook.com>
2015-06-23 23:25:11 +0300

    info-box correction

--- my-works/Info-Box/js/info-box.js
+++ my-works/Info-Box/js/info-box.js
@@ -4,78 +4,87 @@
 	var targetJsonUrl = 'json/info_box.json',
 		contentObjectsFromServer,
 		numberOfContentObjects,
-		indexOfCurrentContentObject,
-		indexOfFirstContentObject,
+		indexOfCurrentContentObject = 0,
+		indexOfFirstContentObject = 0,
 		indexOfLastContentObject,
 		imgNode,
-		duration;
+		duration = 400,
+		$content = $('.content'),
+		$descr = $('.description'),
+		$descrNote = $('.description-note'),
+		$linkDet = $('.link-det'),
+		$title = $('.title'),
+		$note = $('.note'),
+		$productImg = $('.product-img'),
+		$info = $('.info');
+
+	// adding hover effects for buttons
+
+	function inOut(node, classOutBtn, classInBtn, classOutArrow, classInArrow) {
+		$(node).hover(function() {
+			$(this).removeClass(classOutBtn);
+			$(this).addClass(classInBtn);
+			$(this).find('.control-btn').addClass('text-shadow');
+			if (classOutArrow && classInArrow) {
+				$(this).find('.arrow').removeClass(classOutArrow);
+				$(this).find('.arrow').addClass(classInArrow);
+			}
+		}, function() {
+			$(this).removeClass(classInBtn);
+			$(this).addClass(classOutBtn);
+			$(this).find('.control-btn').removeClass('text-shadow');
+			if (classOutArrow && classInArrow) {
+				$(this).find('.arrow').removeClass(classInArrow);
+				$(this).find('.arrow').addClass(classOutArrow);
+			}
+		});
+	}
+
+	inOut($('.button-btn-bg-white-right'),
+		'button-btn-bg-white-right',
+		'button-btn-bg-orange-right',
+		'button-btn-ic-gray-right',
+		'button-btn-ic-brn-right');
+
+	inOut($('.button-btn-bg-white-left'),
+		'button-btn-bg-white-left',
+		'button-btn-bg-orange-left',
+		'button-btn-ic-gray-left',
+		'button-btn-ic-brn-left');
+
+	inOut($('.button-btn-lg-bg'),
+		'button-btn-lg-bg',
+		'button-btn-lg-orange');
+
+	// hover effects were added
+
+	function displayContent(indexOfCurrentContentObject) {
+		$descrNote.removeClass('description-note-collapse');
+		$linkDet.hide().fadeIn(duration);
+		$title.text(contentObjectsFromServer[indexOfCurrentContentObject].title).hide().fadeIn(duration);
+		$descr.text(contentObjectsFromServer[indexOfCurrentContentObject].description).hide().fadeIn(duration);
+		$note.text(contentObjectsFromServer[indexOfCurrentContentObject].note).hide().fadeIn(duration);
+		imgNode = '<img src="imgs/' + contentObjectsFromServer[indexOfCurrentContentObject].img + '">';
+		$productImg.html(imgNode).hide().fadeIn(duration);
+	};
+
+	function setDefault() {
+		$descrNote.removeClass('expand');
+		$descrNote.css('height', '29px');
+		$descrNote.addClass('collapse');
+		$linkDet.text('show details');
+	}
+
+	displayContent(indexOfFirstContentObject);
+
+	// get content from server
 
 	$.getJSON(targetJsonUrl, function(data) {
 		contentObjectsFromServer = data;
 		numberOfContentObjects = contentObjectsFromServer.length;
 		indexOfLastContentObject = contentObjectsFromServer.length - 1;
-		indexOfCurrentContentObject = 0;
-		indexOfFirstContentObject = 0;
-		duration = 400;
-
-		// adding hover effects for buttons
-
-		function inOut(node, classOutBtn, classInBtn, classOutArrow, classInArrow) {
-			$(node).hover(function() {
-				$(this).removeClass(classOutBtn);
-				$(this).addClass(classInBtn);
-				$(this).find('.control-btn').addClass('text-shadow');
-				if (classOutArrow && classInArrow) {
-					$(this).find('.arrow').removeClass(classOutArrow);
-					$(this).find('.arrow').addClass(classInArrow);
-				}
-			}, function() {
-				$(this).removeClass(classInBtn);
-				$(this).addClass(classOutBtn);
-				$(this).find('.control-btn').removeClass('text-shadow');
-				if (classOutArrow && classInArrow) {
-					$(this).find('.arrow').removeClass(classInArrow);
-					$(this).find('.arrow').addClass(classOutArrow);
-				}
-			});
-		}
-
-		inOut($('.button-btn-bg-white-right'),
-			'button-btn-bg-white-right',
-			'button-btn-bg-orange-right',
-			'button-btn-ic-gray-right',
-			'button-btn-ic-brn-right');
-
-		inOut($('.button-btn-bg-white-left'),
-			'button-btn-bg-white-left',
-			'button-btn-bg-orange-left',
-			'button-btn-ic-gray-left',
-			'button-btn-ic-brn-left');
-
-		inOut($('.button-btn-lg-bg'),
-			'button-btn-lg-bg',
-			'button-btn-lg-orange');
 
 		// adding events for buttons
-
-		function displayContent(indexOfCurrentContentObject) {
-			$('.description-note').removeClass('description-note-collapse');
-			$('.link-det').hide().fadeIn(duration);
-			$('.title').text(contentObjectsFromServer[indexOfCurrentContentObject].title).hide().fadeIn(duration);
-			$('.description').text(contentObjectsFromServer[indexOfCurrentContentObject].description).hide().fadeIn(duration);
-			$('.note').text(contentObjectsFromServer[indexOfCurrentContentObject].note).hide().fadeIn(duration);
-			imgNode = '<img src="imgs/' + contentObjectsFromServer[indexOfCurrentContentObject].img + '">';
-			$('.product-img').html(imgNode).hide().fadeIn(duration);
-		};
-
-		function setDefault() {
-			$('.description-note').removeClass('expand');
-			$('.description-note').css('height', '29px');
-			$('.description-note').addClass('collapse');
-			$('.link-det').text('show details');
-		}
-
-		displayContent(indexOfFirstContentObject);
 
 		$('.button-btn-bg-white-right').on('click', function() {
 			setDefault();
@@ -100,23 +109,31 @@
 		});
 
 		$('.show-det').on('click', function() {
-			if ($('.description-note').hasClass('expand')) {
-				$('.link-det').text('show details');
-				$('.description-note').animate({
+			if ($descrNote.hasClass('expand')) {
+				$linkDet.text('show details');
+				$info.addClass('info-absolute');
+				$info.animate({
+					top: '205px'
+				}, duration);
+				$descrNote.animate({
 					height: '29px'
 				}, duration, function() {
-					$('.description-note').removeClass('expand');
-					$('.description-note').addClass('collapse');
-					$('.product-img img').fadeToggle(duration + 300);
+					$descrNote.removeClass('expand');
+					$descrNote.addClass('collapse');
+					$content.find('img').fadeToggle(duration);
 				});
 			} else {
-				$('.product-img img').fadeToggle(duration, function() {
-					$('.link-det').text('hide details');
-					$('.description-note').animate({
+				$content.find('img').fadeToggle(duration, function() {
+					$linkDet.text('hide details');
+					$info.removeClass('info-absolute');
+					$info.animate({
+						top: '0px'
+					}, duration);
+					$descrNote.animate({
 						height: '232px'
 					}, duration, function() {
-						$('.description-note').removeClass('collapse');
-						$('.description-note').addClass('expand');
+						$descrNote.removeClass('collapse');
+						$descrNote.addClass('expand');
 					});
 				});
 			}
@@ -126,4 +143,30 @@
 			$(this).attr('href', contentObjectsFromServer[indexOfCurrentContentObject].productUrl);
 		});
 	});
+
+	// contentObjectsFromServer = [{
+	// 	"title": "Time to Share: 6 for $3.99*",
+	// 	"img": "comp_plate_promo1.png",
+	// 	"description": "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
+	// 	"note": "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
+	// 	"productUrl": "products/promo1.html"
+	// }, {
+	// 	"title": "Rise 'n shine",
+	// 	"img": "comp_plate_promo2.png",
+	// 	"description": "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
+	// 	"note": "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
+	// 	"productUrl": "products/promo2.html"
+	// }, {
+	// 	"title": "PM Snackers: Brownie Bites",
+	// 	"img": "comp_plate_promo3.png",
+	// 	"description": "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
+	// 	"note": "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
+	// 	"productUrl": "products/promo3.html"
+	// }, {
+	// 	"title": "ICED Coffee",
+	// 	"img": "comp_plate_promo4.png",
+	// 	"description": "Lorem ipsum dolor sit amet. consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exefcitalion ullamoo laboris nisi ut aliquip ex ea commodo oonsequat.",
+	// 	"note": "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga. * At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.\n* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.\n* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
+	// 	"productUrl": "products/promo4.html"
+	// }];
 })();
